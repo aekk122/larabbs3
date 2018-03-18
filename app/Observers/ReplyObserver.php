288f8f344da69e9c22ef24bd7b0776d4cc5defb2
vagrant <vagrant@homestead>
2018-03-18 06:07:52 +0000

    Notify author when topic replied

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -16,7 +17,11 @@ class ReplyObserver
     }
 
     public function created(Reply $reply) {
-    	$reply->belongsToTopic->increment('reply_count', 1);
+    	$topic = $reply->belongsToTopic;
+    	$topic->increment('reply_count', 1);
+
+    	// 通知作者回复
+    	$topic->belongsToUser->notify(new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,5 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destory']]);
+
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
