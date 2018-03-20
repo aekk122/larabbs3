0608ac84a99e8ee083b673d6784feb3ea52c3dd6
vagrant <vagrant@homestead>
2018-03-20 06:45:04 +0000

    Sidebar active users

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
 
 class CategoriesController extends Controller
 {
     //
-    public function show(Request $request, Category $category, Topic $topic) {
+    public function show(Request $request, Category $category, Topic $topic, User $user) {
     	// 读取分类 ID 关联话题，按 20 每页分页
     	$topics = $topic->withOrder($request->order)->where('category_id', $category->id)->paginate(20);
 
+    	$active_users = $user->getActiveUsers();
     	// 传参
-    	return view('topics.index', compact('topics', 'category'));
+    	return view('topics.index', compact('topics', 'category', 'active_users'));
     }
 }
